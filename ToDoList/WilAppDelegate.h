d2274b4a03c587e1e227b8ed43aa6422248b3888
wilsonposada <wilsonposada@me.com>
2014-04-23 21:45:18 -0500

    Initial Commit

---
+++ ToDoList/WilAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WilAppDelegate.h
+//  ToDoList
+//
+//  Created by ChrisMacol on 4/23/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WilAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by ChrisMacol on 4/23/14.
+//  Copyright (c) 2014 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WilAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WilAppDelegate class]));
+    }
+}
